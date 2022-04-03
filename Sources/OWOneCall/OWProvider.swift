5a1ee70d4c7dad6c305a0d84a802e5854ab80230
workingdog <ringowathelet@gmail.com>
2022-04-03 09:26:49 +0900

    added convenience callback

--- Sources/OWOneCall/OWProvider.swift
+++ Sources/OWOneCall/OWProvider.swift
@@ -28,12 +28,23 @@ open class OWProvider {
         }
     }
     
+    /// get the weather at the given location with the given options, with async
     open func getWeather(lat: Double, lon: Double, options: OWOptionsProtocol) async -> OWResponse? {
         do {
             let results: OWResponse = try await client.fetchThisAsync(param: "lat=\(lat)&lon=\(lon)", options: options)
             return results
         } catch {
             return nil
+        }
+    }
+    
+    /// convenience method, get the weather at the given location with the given options, with callback
+    open func getWeather(lat: Double, lon: Double, options: OWOptionsProtocol, completion: @escaping (OWResponse?) -> Void) {
+        Task {
+            let results: OWResponse? = await getWeather(lat: lat, lon: lon, options: options)
+            DispatchQueue.main.async {
+                completion(results)
+            }
         }
     }
     
